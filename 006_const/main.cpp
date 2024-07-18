8153eca9b62fa3be3e8bce53a53af89468c862f9
Alexander Zarubin <whitesashka@gmail.com>
2024-07-18 11:58:03 +0500

    upadate encoding

---
+++ 006_const/main.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	const int COUNT_DAYS_IN_WEEl = 7;
+	const char NEW_LINE = '\n';
+	cout << COUNT_DAYS_IN_WEEl << NEW_LINE;
+
+	return 0;
+}
