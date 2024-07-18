3deaa2d15b7f7ee72f422bf4d41b1163caf9a602
Alexander Zarubin <whitesashka@gmail.com>
2024-07-18 12:03:50 +0500

    add 007_cin_cout

---
+++ 007_cin_cout/main.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int Var;
+	cout << "Введите число ";
+	cin >> Var;
+	cout << "Вы ввели число " << Var << endl;
+	return 0;
+}
