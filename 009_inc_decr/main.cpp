651289641d1115133494ed498fef97b76c3e6127
Alexander Zarubin <whitesashka@gmail.com>
2024-07-18 12:19:46 +0500

    add 009_inc_decr

---
+++ 009_inc_decr/main.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	int a = 1, b;
+	//a = a + 5;
+	cout << a << endl;
+	b = ++a * a++;
+	cout << b << endl;
+	cout << a << endl;
+	//посвфиксная a++ сначала выводит потом деиствие посфиксная после 
+	//префиксаная ++a сначала деиствие потом вывод   префиксная перед
+}
