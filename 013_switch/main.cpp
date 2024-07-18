c349e0c8c65e5e43a7190f096556ae521dc0aede
Alexander Zarubin <whitesashka@gmail.com>
2024-07-18 13:57:21 +0500

    add 013_switch update 012_if_else

---
+++ 013_switch/main.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+//#define WORK
+#define calc
+using namespace std;
+//Оператор switch
+void main()
+{
+	setlocale(LC_ALL, "rus");
+#ifdef WORK
+	int a;
+	cout << "enter the number " << endl;
+	cin >> a;
+
+	switch (a)
+	{
+	case 1:
+		cout << "you introducet one " << endl;
+		break;
+	case 2:
+		cout << "you introducet two " << endl;
+		break;
+	default:
+		cout << "i don't know this numbers " << endl;
+		break;
+	}
+#endif // WORK
+
+
+#ifdef calc
+	double a = 0.0, b = 0.0;
+
+	char action = ' ';
+	if (true)
+	{
+		cout << "my calculator" << endl;
+		cout << "enter the number 1 "; cin >> a;
+		cout << "enter action "; cin >> action;
+		cout << "enter the number 2 "; cin >> b;
+		cout << endl;
+		switch (action)
+		{
+		case '+':
+			cout << "a + b = " << a + b << endl;
+			break;
+		case '-':
+			cout << "a - b = " << a - b << endl;
+			break;
+		case '*':
+			cout << "a * b = " << a * b << endl;
+			break;
+		case '/':
+			cout << "a / b = " << (double)(a / b) << endl;
+			break;
+		default:
+			break;
+		}
+
+	}
+
+#endif // calc
+
+
+}
