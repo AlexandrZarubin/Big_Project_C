491b79f2ec2f5c0d0ac7d3e58660e0ee7de390c8
Alexander Zarubin <whitesashka@gmail.com>
2024-07-25 01:02:44 +0500

    add 039_inline

---
+++ 039_inline/main.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+// inline
+
+inline int Sum(int a, int b);
+
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << Sum(5, 5);
+
+}
+
+int Sum(int a, int b)
+{
+	return a + b;
+}
