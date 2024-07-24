e725561e5c991831ed7650f2879bcd848a0ed2ce
Alexander Zarubin <whitesashka@gmail.com>
2024-07-24 23:05:29 +0500

    add 035-037

---
+++ 035_Functions_array/main.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void FillArray(int arr[], const int size)
+{
+	int a = sizeof(arr);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+void Print(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	const int SIZE = 10;
+	int arr[SIZE]{};
+	int a = sizeof(arr);
+	FillArray(arr, SIZE);
+	Print(arr, SIZE);
+
+}
---
+++ 037_Area_Of_Visibility/main.cpp
@@ -0,0 +1,23 @@
+﻿
+//					Область видимости
+
+#include<iostream>
+
+using namespace std;
+int c;
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	c++;
+	cout << c << endl;
+	int a = 5;
+	{
+		c++;
+		cout << c << endl;
+		int a = 5;
+		int b = 3;
+	}
+	c++;
+	cout << c << endl;
+	//b = 3;
+}
