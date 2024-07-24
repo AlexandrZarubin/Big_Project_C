a9d4d4503b92cbd372d03dd28a815239891d517d
Alexander Zarubin <whitesashka@gmail.com>
2024-07-25 01:04:47 +0500

    add 040_Overload_functions

---
+++ 040_Overload_Functionc/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int Sum(int a, int b, int c)
+{
+	a++;
+	return a + b + c;
+}
+int Sum(int a, int b)
+{
+	return a + b;
+}
+
+double Sum(double a, double b)
+{
+	return a + b;
+}
+void main()
+{
+	cout << Sum(1, 2, 3) << endl;
+	cout << Sum(1, 2) << endl;
+	cout << Sum(1.5, 2.2) << endl;
+}
