9996a8ee87683be7b00fea287b82899c571e9eae
Alexander Zarubin <whitesashka@gmail.com>
2024-07-25 23:41:08 +0500

    add 047_Arhmetic_Pointer

---
+++ 047_Arithmetic_Pointer/main.cpp
@@ -0,0 +1,34 @@
+﻿
+/*Указатели и массивы. Арифметика указателей*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const int SIZE = 5;
+
+    int arr[SIZE]{ 5,6,8,9,4 };
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    cout << "================================" << endl;
+    int* pArr = arr;
+    cout << "arr\t" << arr << endl;
+    cout << "pArr\t" << pArr << endl;
+    cout << "*arr\t" << *arr << endl;
+    cout << "*pArr\t" << *pArr << endl;
+    cout << "arr[i=2]  " << *(arr + 2) << endl;
+    cout << "================================" << endl;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        //cout << arr[i] << endl;
+        cout << *(arr + i) << endl;
+    }
+
+
+}
+
